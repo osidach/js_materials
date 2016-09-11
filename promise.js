08c0f6db50c71b28bc6be195216d16a4844ae073
osidach <osidach@gmail.com>
2016-09-12 01:30:40 +0300

    Update promise.js

--- promise.js
+++ promise.js
@@ -1,3 +1,4 @@
+// ----------------- Promise -----------------
 var ajax = function(url) {
     return new Promise(function(resolve, reject) {
         var client = new XMLHttpRequest();
@@ -22,10 +23,8 @@ ajax('http://example.com/get').then(function(data) {
     console.log('Error: ', error);
 });
 
-// no chance to use "throw 'Error';" in promise because it's an asynchronous operation
-// try - throw - catch chain works only in the synchronous operations
-
-var time = function(url) {
+// ----------------- Promise Catch -----------------
+var time = function() {
     return new Promise(function(resolve, reject) {
         setTimeout(function() {
             // throw 'Error in asynchronous promise';
@@ -34,6 +33,8 @@ var time = function(url) {
         }, 3000);
     });
 };
+// no chance to use "throw 'Error';" in promise because it's an asynchronous operation
+// try - throw - catch chain works only in the synchronous operations
 
 time().then(function(data) {
     console.log('Resolved: ', data);
@@ -44,4 +45,44 @@ time().then(function(data) {
     console.log('Error: ', error);
 }).catch(function(error) {
     console.log('Error: ', error.msg);
+});
+
+// ----------------- Promise All Reject -----------------
+var promise1 = new Promise(function(resolve, reject) {
+    setTimeout(function() {
+        resolve('success');
+    }, 3000);
+});
+var promise2 = function() new Promise(function(resolve, reject) {
+    setTimeout(function() {
+        reject('error');
+    }, 3000);
+});
+var promise3 = Promise.reject('error2');
+
+// Promise.all is rejected if one of the elements is rejected
+Promise.all([promise1, promise2, promise3]).then(function(dataArr) {
+    console.log('Resolve array: ', dataArr);
+}, function(dataErr) {
+    console.log('Reject array: ', dataErr);
+});
+
+// ----------------- Promise All Resolve -----------------
+var promise1 = new Promise(function(resolve, reject) {
+    setTimeout(function() {
+        resolve('success1');
+    }, 3000);
+});
+var promise2 = new Promise(function(resolve, reject) {
+    setTimeout(function() {
+        resolve('success2');
+    }, 6000);
+});
+
+// Promise.all is resolved when all promises are resolved 
+// (or if anyone of them is rejected)
+Promise.all([promise1, promise2]).then(function(dataArr) {
+    console.log('Resolve array: ', dataArr);
+}, function(dataErr) {
+    console.log('Reject array: ', dataErr);
 });
