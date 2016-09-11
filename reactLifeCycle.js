83b1f916edcfdf720b819f480bf409ba6c79d79a
osidach <osidach@gmail.com>
2016-09-12 02:24:47 +0300

    Create reactLifeCycle.js

---
+++ reactLifeCycle.js
@@ -0,0 +1,22 @@
+1. Initial Render:
+    getDefaultProps()
+    getInitialState()
+    componentWillMount()
+    render()
+    componentDidMount()
+
+2. Props Change:
+    componentWillRecieveProps()
+    shouldComponentUpdate()
+    componentWillUpdate()
+    render()
+    componentDidUpdate()
+
+3. State Change:
+    shouldComponentUpdate()
+    componentWillUpdate()
+    render()
+    componentDidUpdate()
+
+4. Component Unmount:
+    willUnmount()
