ce62ce11ece4b2f24a2b658d08be63d13d897295
osidach <osidach@gmail.com>
2016-09-12 02:19:24 +0300

    Update curryPolifill.js

--- curryPolifill.js
+++ curryPolifill.js
@@ -1,1 +1,8 @@
-
+var curry = function(fn) {
+    var args = Array.prototype.slice.call(arguments, 1);
+    return function() {
+        var internalArgs = Array.prototype.slice.call(arguments),
+            newArgs = args.concat(internalArgs);
+            return fn.call(null, newArgs);
+    };
+};
