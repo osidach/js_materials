6e4225c721ea3a3e3e33492877264bd726a96515
osidach <osidach@gmail.com>
2016-09-12 01:04:49 +0300

    Update promise.js

--- promise.js
+++ promise.js
@@ -24,3 +24,24 @@ ajax('http://example.com/get').then(function(data) {
 
 // no chance to use "throw 'Error';" in promise because it's an asynchronous operation
 // try - throw - catch chain works only in the synchronous operations
+
+var time = function(url) {
+    return new Promise(function(resolve, reject) {
+        setTimeout(function() {
+            // throw 'Error in asynchronous promise';
+            // Will be "Uncaught Error in asynchronous promise" in then()
+            resolve('success');
+        }, 3000);
+    });
+};
+
+time().then(function(data) {
+    console.log('Resolved: ', data);
+    throw {
+        msg: 'Something happened in resolve'
+    };
+}, function(error) {
+    console.log('Error: ', error);
+}).catch(function(error) {
+    console.log('Error: ', error.msg);
+});
